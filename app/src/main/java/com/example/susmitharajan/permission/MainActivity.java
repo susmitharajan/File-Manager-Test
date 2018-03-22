016798cf7b58a49e9eb03ee64e65cb7fc08d6d70
susmitharajan <susmithasundararajan6@gmail.com>
2018-03-22 18:56:04 +0530

    First Commit

--- app/src/main/java/com/example/susmitharajan/permission/MainActivity.java
+++ app/src/main/java/com/example/susmitharajan/permission/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
+import android.os.StatFs;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.os.EnvironmentCompat;
@@ -20,8 +22,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+import static android.provider.Telephony.ThreadsColumns.ERROR;
+
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +41,29 @@ public class MainActivity extends AppCompatActivity {
                File root = new File(externalStorageDirectories[0]);
                 List<File> getListFiles2 = getListFiles2(root);
 
+
+                //Available Internal Memory
+                StatFs stat = new StatFs(Environment.getExternalStorageDirectory().getPath());
+                long bytesAvailable;
+                if (android.os.Build.VERSION.SDK_INT >=
+                        android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                    bytesAvailable = stat.getBlockSizeLong() * stat.getAvailableBlocksLong();
+                }
+                else {
+                    bytesAvailable = (long)stat.getBlockSize() * (long)stat.getAvailableBlocks();
+                }
+                long megAvailable = bytesAvailable / (1024 * 1024);
+                Log.e("","Available MB : "+megAvailable);
+
             }
         }
+
+
+        String AvailableInternalMemory = getAvailableInternalMemorySize();
+        String getTotalInternalMemorySize = getTotalInternalMemorySize();
+        String getAvailableExternalMemorySize = getAvailableExternalMemorySize();
+        String getTotalExternalMemorySize = getTotalExternalMemorySize();
+
     }
 
     private boolean checkIfAlreadyhavePermission() {
@@ -163,5 +189,92 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0; i<results.size(); ++i) storageDirectories[i] = results.get(i);
 
         return storageDirectories;
+    }
+
+
+
+    //Find out Memory Space
+
+    public static boolean externalMemoryAvailable() {
+        return android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static String getAvailableInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSizeLong();
+        long availableBlocks = stat.getAvailableBlocksLong();
+        return formatSize(availableBlocks * blockSize);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static String getTotalInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSizeLong();
+        long totalBlocks = stat.getBlockCountLong();
+        return formatSize(totalBlocks * blockSize);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public String getAvailableExternalMemorySize() {
+        if (externalMemoryAvailable()) {
+           // File path = Environment.getExternalStorageDirectory();
+            String[] externalStorageDirectories = getExternalStorageDirectories();
+            Log.d("MainActivity",externalStorageDirectories[0]);
+            File root = new File(externalStorageDirectories[0]);
+            StatFs stat = new StatFs(root.getPath());
+            long blockSize = stat.getBlockSizeLong();
+            long availableBlocks = stat.getAvailableBlocksLong();
+            return formatSize(availableBlocks * blockSize);
+        } else {
+            return ERROR;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public String getTotalExternalMemorySize() {
+        if (externalMemoryAvailable()) {
+          //  File path = Environment.getExternalStorageDirectory();
+            String[] externalStorageDirectories = getExternalStorageDirectories();
+            Log.d("MainActivity",externalStorageDirectories[0]);
+            File root = new File(externalStorageDirectories[0]);
+            StatFs stat = new StatFs(root.getPath());
+            long blockSize = stat.getBlockSizeLong();
+            long totalBlocks = stat.getBlockCountLong();
+            return formatSize(totalBlocks * blockSize);
+        } else {
+            return ERROR;
+        }
+    }
+
+    public static String formatSize(long size) {
+        String suffix = null;
+
+        if (size >= 1024) {
+            suffix = "KB";
+            size /= 1024.0;
+            if (size >= 1024) {
+                suffix = "MB";
+                size /= 1024.0;
+                if(size >= 1024){
+                    suffix = "GB";
+                    size /= 1024.0;
+                }
+            }
+        }
+
+        StringBuilder resultBuffer = new StringBuilder(Long.toString(size));
+
+        int commaOffset = resultBuffer.length() - 3;
+        while (commaOffset > 0) {
+            resultBuffer.insert(commaOffset, ',');
+            commaOffset -= 3;
+        }
+
+        if (suffix != null) resultBuffer.append(suffix);
+        return resultBuffer.toString();
     }
 }
